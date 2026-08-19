4ae87a6114cef4100ae437d7c58ecd3c17c7c765
syedarshaanali12-cpu <syedarshaanali12@gmail.com>
2026-08-20 03:41:29 +0530

    Fix opening animation dev lock caused by StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
@@ -6,9 +5,7 @@ import './styles.css';
 import './admin.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
